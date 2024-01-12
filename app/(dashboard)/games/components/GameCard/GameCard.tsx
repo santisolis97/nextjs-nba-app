0ff32825a761703057af0179edc549aa9ac37cf2
Santiago Solis <santiagosolis@MacBook-Pro-de-Santiago.local>
2024-01-12 13:12:49 -0300

    added tailwind sorter

--- app/(dashboard)/games/components/GameCard/GameCard.tsx
+++ app/(dashboard)/games/components/GameCard/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Game } from '@/app/types/games';
-import Image from 'next/image';
-import 'react-loading-skeleton/dist/skeleton.css';
+"use client";
+import { Game } from "@/app/types/games";
+import Image from "next/image";
+import "react-loading-skeleton/dist/skeleton.css";
 export const GameCard = ({
   date,
   home_team,
@@ -11,60 +12,89 @@ export const GameCard = ({
   visitor_team_score,
 }: Game) => {
   const isHomeTeamWinner = home_team_score > visitor_team_score;
-  const isVisitorTeamWinner = visitor_team_score > home_team_score;
+
+  const originalDate = new Date(date);
+  const formattedDate = `${originalDate.getFullYear()}-${(
+    originalDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}-${originalDate.getDate().toString().padStart(2, "0")}`;
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(event.detail);
+    switch (event.detail) {
+      case 1: {
+        console.log("single click");
+        break;
+      }
+      case 2: {
+        console.log("double click");
+        break;
+      }
+      case 3: {
+        console.log("triple click");
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
   return (
-    <div className=' hover:scale-105 transition-transform justify-between max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white dark:bg-gray-800 p-6  h-96 sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col '>
-      <div className='flex justify-between mb-4'>
-        <div className={`text-lg font-semibold max-w-[90%]`}>
+    <button
+      onClick={handleClick}
+      className=" flex h-96 max-w-sm flex-col items-center justify-between overflow-hidden rounded-3xl bg-white p-6 shadow-lg  transition-transform hover:scale-105 sm:w-1/2 md:w-1/3 lg:w-1/4 dark:bg-gray-800 "
+    >
+      <div className="mb-4 flex justify-between">
+        <div className={`max-w-[90%] text-lg font-semibold`}>
           <span
             className={`${
-              isHomeTeamWinner ? 'text-green-400' : 'text-red-400'
+              isHomeTeamWinner ? "text-green-400" : "text-red-400"
             }`}
           >
             {home_team.full_name}
-          </span>{' '}
-          @{' '}
+          </span>{" "}
+          @{" "}
           <span
             className={`${
-              isVisitorTeamWinner ? 'text-green-400' : 'text-red-400'
+              !isHomeTeamWinner ? "text-green-400" : "text-red-400"
             }`}
           >
             {visitor_team.full_name}
           </span>
         </div>
-        <div className='text-gray-500'>{status}</div>
+        <div className="text-gray-500">{status}</div>
       </div>
 
-      <div className='mb-4'>
-        <div className='text-sm text-gray-500'>Date</div>
-        <div className='text-lg'>{new Date(date).toLocaleDateString()}</div>
+      <div className="mb-4">
+        <div className="text-sm text-gray-500">Date</div>
+        <div className="text-lg">{formattedDate}</div>
       </div>
 
-      <div className='mb-4'>
-        <div className='text-sm text-gray-500'>Score</div>
-        <div className='text-lg'>
-          {home_team.full_name} {home_team_score} - {visitor_team_score}{' '}
+      <div className="mb-4">
+        <div className="text-sm text-gray-500">Score</div>
+        <div className="text-lg">
+          {home_team.full_name} {home_team_score} - {visitor_team_score}{" "}
           {visitor_team.full_name}
         </div>
       </div>
 
-      <div className='mb-4'>
-        <div className='text-sm text-gray-500'>Period</div>
-        <div className='text-lg'>{period}</div>
+      <div className="mb-4">
+        <div className="text-sm text-gray-500">Period</div>
+        <div className="text-lg">{period}</div>
       </div>
 
       {/* Add more details as needed */}
 
-      <div className='flex justify-center '>
+      <div className="flex justify-center ">
         <Image
-          src='/nba-logo.webp'
-          alt='NBA Logo'
-          width='0'
-          height='0'
-          sizes='100vw'
-          className='w-[96px] h-[54px]'
+          src="/nba-logo.webp"
+          alt="NBA Logo"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="h-[54px] w-[96px]"
         />
       </div>
-    </div>
+    </button>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,39 @@
-import Image from 'next/image';
-import { Link } from './components/Link/Link';
-import { FaArrowRight } from 'react-icons/fa';
+import Image from "next/image";
+import { Link } from "./components/Link/Link";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <main className='flex-1'>
-        <section className='relative w-full h-[100vh]'>
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-1">
+        <section className="relative h-[100vh] w-full">
           <Image
-            alt='NBA Game'
-            className='absolute inset-0 object-cover w-full h-full'
-            height='1080'
-            src='https://cdn.nba.com/manage/2023/02/IMG_9200-1568x882.jpg'
+            priority
+            alt="NBA Game"
+            className="absolute inset-0 h-full w-full object-cover"
+            height="1080"
+            src="https://cdn.nba.com/manage/2023/02/IMG_9200-1568x882.jpg"
             style={{
-              aspectRatio: '1920/1080',
-              objectFit: 'cover',
+              aspectRatio: "1920/1080",
+              objectFit: "cover",
             }}
-            width='1920'
+            width="1920"
           />
-          <div className='absolute inset-0 bg-black/70' />
-          <div className='relative z-10 flex flex-col items-center justify-center h-full px-4 text-center'>
-            <h1 className='text-3xl font-bold text-white sm:text-4xl md:text-5xl'>
+          <div className="absolute inset-0 bg-black/70" />
+          <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center">
+            <h1 className="text-3xl font-bold text-white sm:text-4xl md:text-5xl">
               Experience the Thrill of NBA
             </h1>
-            <p className='mt-4 text-lg text-gray-300 md:text-xl pb-8'>
+            <p className="mt-4 pb-8 text-lg text-gray-300 md:text-xl">
               Join the ultimate NBA fan community and get access to exclusive
               content, stats, and more.
             </p>
             <Link
-              className='p-4 bg-gray-50 dark:hover:bg-gray-300'
-              href='/games'
+              className="bg-gray-50 p-4 dark:hover:bg-gray-300"
+              href="/games"
             >
-              <div className='flex items-center gap-2'>
-                Go to Dashboard <FaArrowRight />{' '}
+              <div className="flex items-center gap-2">
+                Go to Dashboard <FaArrowRight />{" "}
               </div>
             </Link>
           </div>
